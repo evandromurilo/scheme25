8c627610ac7116bc8ecbd348cda6220906e6026b
murilo <evandromurilob@hotmail.com>
2025-08-26 19:30:27 -0400

    add to-binary

--- binary.scm
+++ binary.scm
@@ -12,3 +12,24 @@
 	  (else (+ (* (first rbinum) scale)
 	     (helper (bf rbinum) (* scale 2))))))
   (helper (reverse-term binum) 1))
+
+(define (to-binary dec)
+  ; acha o maior multiplo de 2 que nao ultrapassa dec
+  (define (find-scale dec scale)
+    (cond ((> dec scale)
+	   (find-scale dec (* scale 2)))
+	  ((= dec scale)
+	   scale)
+	  (else
+	   (/ scale 2))))
+
+  (define (helper dec scale)
+    (cond ((< scale 1)
+	   (word))
+	  ((>= dec scale)
+	   (word 1 (helper (- dec scale) (/ scale 2))))
+	  (else
+	   (word 0 (helper dec (/ scale 2))))))
+
+  (helper dec (find-scale dec 1)))
+
