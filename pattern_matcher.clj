da0ae7a91ee8a0c0b0bd9a91169bed05245cec66
murilo <evandromurilob@hotmail.com>
2025-10-26 12:07:38 -0400

    pattern matcher ? and !

--- pattern_matcher.clj
+++ pattern_matcher.clj
@@ -1,5 +1,10 @@
 ;; follow up with the pattern matcher example on simply scheme book
 ;; but implemented in clojure for fun
+;;
+;; ? At most one word
+;; ! Exactly one word
+;; & At least one word
+;; * Any number of words
 
 ;; but-first, like in simply.scm
 
@@ -12,4 +17,14 @@
     (= (first sent1) (first sent2)) (sent-equal? (bf sent1) (bf sent2))
     :else false))
   
-	
+(defn match? [pattern sent]
+  (cond
+    (empty? pattern) (empty? sent)
+    (empty? sent) false
+    (= (first pattern) '!) (match? (bf pattern) (bf sent))
+    (= (first pattern) '?) (if (empty? sent)
+                             (match? (bf pattern) '()) ;; empty sentence, will match if empty patterna after ?
+                             (or (match? (bf pattern) (bf sent)) ;; otherwise will match the first word for ?, or just skip the ?
+                                 (match? (bf pattern) sent)))
+    (= (first pattern) (first sent)) (match? (bf pattern) (bf sent))
+    :else false))
