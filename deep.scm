d70bff70a0b68df81e6d151f95045e3bf52572fa
Evandro Murilo <evandromurilob@hotmail.com>
2025-11-23 09:46:17 -0400

    add exercises from chapter 17 of simply scheme, valid-infix?

--- deep.scm
+++ deep.scm
@@ -41,3 +41,38 @@
 	 (flatten (cons (caar l) (cons (cdar l) (cdr l)))))
 	(else (cons (car l) (flatten (cdr l))))))
 
+(define (deep-count lst)
+  (cond ((null? lst) 0)
+	((word? lst) 1)
+	(else (+ (deep-count (car lst))
+		 (deep-count (cdr lst))))))
+
+(define (branch target lst)
+  (if (null? target)
+      lst
+      (branch (cdr target) (item (car target) lst))))
+
+;; (valid-infix? '(1 + 1 * (2 * 5) - 5 - (3 - (2 * 4))))
+(define (valid-infix? lst)
+  (define (operator? obj)
+    (member obj '(+ - * /)))
+  (define (operand? obj)
+    (or (number? obj)
+	(list? obj)))
+  (define (valid-from-operator? lst)
+    (or (null? lst)
+	(and (operator? (car lst))
+	     (valid-from-operand? (cdr lst)))))
+  (define (valid-from-operand? lst)
+    (cond ((null? lst)
+	   #f)
+	  ((number? (car lst))
+	   (valid-from-operator? (cdr lst)))
+	  ((list? (car lst))
+	   (and (valid-from-operand? (car lst))
+		(valid-from-operator? (cdr lst))))
+	  (else #f)))
+  
+  (valid-from-operand? lst))
+
+
