a34e622109a8fe690e9855edd1ed1fcf7a8d0e5a
Evandro Murilo <evandromurilob@hotmail.com>
2025-12-06 09:10:30 -0400

    true-for-all? true-for-any-pair? true-for-all-pairs?

--- higher.scm
+++ higher.scm
@@ -2,3 +2,24 @@
   (if (null? (cdr lst))
       (car lst)
       (op (left-accumulate op (cdr lst)) (car lst))))
+
+(define (true-for-all? op lst)
+  (if (null? lst)
+      #t
+      (and (op (car lst)) (true-for-all? op (cdr lst)))))
+
+(define (true-for-any-pair? op lst)
+  (if (null? (cdr lst))
+      #f
+      (or
+       (op (car lst) (cadr lst))
+       (true-for-any-pair? op (cdr lst)))))
+
+(define (true-for-all-pairs? op lst)
+  (if (null? (cdr lst))
+      #t
+      (and (op (car lst) (cadr lst))
+	   (true-for-all-pairs? op (cdr lst)))))
+
+(define (true-for-all-pairs? op lst)
+  (not (true-for-any-pair? (lambda (a b) (not (op a b))) lst)))
