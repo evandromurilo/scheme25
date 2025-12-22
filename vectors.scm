0ff4ab5c1f39d54296de9affa3e2696f1dc2bfe4
Evandro Murilo <evandromurilob@hotmail.com>
2025-12-22 12:41:10 -0400

    add vector.scm

---
+++ vectors.scm
@@ -0,0 +1,34 @@
+(define (sum-vector vec)
+  (sum-vector-helper vec (- (vector-length vec) 1)))
+
+(define (sum-vector-helper vec index)
+  (if (< index 0)
+      0
+      (+ (vector-ref vec index) (sum-vector-helper vec (- index 1)))))
+
+(define (vector-fill! vec val)
+  (vector-fill-helper vec val (- (vector-length vec) 1)))
+
+(define (vector-fill-helper vec val index)
+  (if (< index 0)
+      vec
+      (begin
+	(vector-set! vec index val)
+	(vector-fill-helper vec val (- index 1)))))
+
+(define (vector-append a-vec b-vec)
+  (let ((new (make-vector (+ (vector-length a-vec) (vector-length b-vec)))))
+    (vector-copy! new a-vec 0)
+    (vector-copy! new b-vec (vector-length a-vec))
+    new))
+
+(define (vector-copy! vec old-vec from-index)
+  (vector-copy-helper vec old-vec from-index (- (vector-length old-vec) 1)))
+
+(define (vector-copy-helper vec old-vec from-index index)
+  (if (< index 0)
+      'done
+      (begin
+	(vector-set! vec (+ from-index index) (vector-ref old-vec index))
+	(vector-copy-helper vec old-vec from-index (- index 1)))))
+			
